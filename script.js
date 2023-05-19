bd43715a44ad34e3c5cbf75ae62b92836aff0892
João Pedro <JoaoPedro.stack@gmail.com>
2023-05-19 00:12:38 -0300

    a new path was added, /styles, contains all archives .css

---
+++ script.js
@@ -0,0 +1,21 @@
+const buttonLeft = document.querySelector('#scrollLeft')
+
+    const buttonRight = document.querySelector('#scrollRight')
+    
+    let feedback = document.querySelector('.personContainer')
+
+function scrollX(){    
+
+if(feedback.style.transform == 'translateX(0px)'){
+    feedback.style.transform = 'translateX(-700px)'
+    buttonRight.style.filter = 'brightness(30%)'
+    buttonLeft.style.filter = 'brightness(100%)'
+}else{
+    feedback.style.transform = 'translateX(0px)'
+    buttonRight.style.filter = 'brightness(100%)'
+    buttonLeft.style.filter = 'brightness(30%)'
+}
+
+}
+
+window.onload(setInterval(() => {scrollX()}, 3500))
